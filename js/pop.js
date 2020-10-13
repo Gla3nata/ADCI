fa763132b9c9f999610ab6c5dd56c5f76fb8bc8a
Natalya <0207natalya@gmail.com>
2020-10-14 01:04:33 +0600

    finish
    first

--- js/pop.js
+++ js/pop.js
@@ -1,9 +1,11 @@
 let isDouwn = false;
 let box = document.querySelector('.box');
+let arrow = document.querySelector('.slider-arrow');
 
-function hidePop() {   
-    isDouwn = !isDouwn
-    box.style.height = isDouwn ? '179px' :'28px' ; 
+function showPop() {   
+    isDouwn = !isDouwn;
+    box.style.height = isDouwn ? '235px' :'28px' ;
+    arrow.style.transform = isDouwn ? "rotate(180deg)" : "rotate(0)";
 }
 
 //function hidePop() { 
